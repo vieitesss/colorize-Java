8aa91a86f8c4b7e84be29eea8f653572cb2b94b4
Daniel Vieites Torres <dvieitest@gmail.com>
2023-12-07 17:58:36 +0100

    adding source

---
+++ src/color/Color.java
@@ -0,0 +1,23 @@
+package color;
+
+public enum Color {
+  COLOR_RESET("\u001B[0m"),
+  COLOR_BLACK("\u001B[30m"),
+  COLOR_RED("\u001B[31m"),
+  COLOR_GREEN("\u001B[32m"),
+  COLOR_YELLOW("\u001B[33m"),
+  COLOR_BLUE("\u001B[34m"),
+  COLOR_PURPLE("\u001B[35m"),
+  COLOR_CYAN("\u001B[36m"),
+  COLOR_WHITE("\u001B[37m");
+
+  public final String code;
+
+  private Color(String code) {
+    this.code = code;
+  }
+
+  public static String colorize(Color color, String message) {
+    return color.code + message + Color.COLOR_RESET.code;
+  }
+}
